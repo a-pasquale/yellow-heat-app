a66fe967ea8a997a304d5cfe9c52d1f73dcb643b
Andrew Pasquale <andrew@elytra.net>
2018-12-11 09:14:23 -0500

    Add toast notification when user updates notification preferences.

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { Heater } from '../../app/heater';
 import { HeaterService } from '../../app/heater.service';
@@ -22,7 +22,7 @@ export class NotificationsPage {
   public user: User;
   public deviceRef: AngularFireObject<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, afDB: AngularFireDatabase, private userService: UserService, private heaterService: HeaterService,) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, afDB: AngularFireDatabase, private userService: UserService, private heaterService: HeaterService, public toastCtrl: ToastController) {
     this.user = userService.getUser();
     this.heater = heaterService.getHeater();
     this.deviceRef = afDB.object(`/users/${this.user.id}/${this.heater.id}`);
@@ -33,8 +33,16 @@ export class NotificationsPage {
     console.log('ionViewDidLoad NotificationsPage');
   }
   save(notificationLevel, notifyYellowHeat) {
-    console.log("Saving.");
-    this.deviceRef.update( { notificationLevel: notificationLevel, notifyYellowHeat: notifyYellowHeat });
+    this.deviceRef.update( { notificationLevel: notificationLevel, notifyYellowHeat: notifyYellowHeat })
+    .then( 
+      () => {
+        const toast = this.toastCtrl.create({
+          message: 'Notification preferences updated',
+          duration: 3000
+        });
+        toast.present();
+      }
+    );
   }
   updateNotification() {
     console.log("update changed");
